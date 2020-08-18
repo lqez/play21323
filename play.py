465670fc5d1c9b7a4230ff9fa31c6e150f274f9f
Park Hyunwoo <ez.amiryo@gmail.com>
2020-08-19 03:35:21 +0900

    Add release value

--- play.py
+++ play.py
@@ -7,12 +7,14 @@ from pygame.mixer import Sound
 # low buffer for shorter latency
 pygame.mixer.init(22050, -8, 2, 256)
 
+# Mux selector
 signal = [
     LED('BOARD11'),
     LED('BOARD13'),
     LED('BOARD15'),
 ]
 
+# Muxes are connected to each touch sensor
 mux = [
     Button('BOARD35', pull_up=False),
     Button('BOARD33', pull_up=False),
@@ -21,53 +23,76 @@ mux = [
 ]
 
 print("Loading sounds...")
-soundmap = [
-    # MUX 0
-    {"name": "F3", "sound": Sound("assets/f3.wav"), },
-    {"name": "C3", "sound": Sound("assets/c3.wav"), },
-    {"name": "G3", "sound": Sound("assets/g3.wav"), },
-    {"name": "C#3", "sound": Sound("assets/c3s.wav"), },
-    {"name": "F#3", "sound": Sound("assets/f3s.wav"), },
-    {"name": "D#3", "sound": Sound("assets/d3s.wav"), },
-    {"name": "E3", "sound": Sound("assets/e3.wav"), },
-    {"name": "D3", "sound": Sound("assets/d3.wav"), },
 
-    # MUX 1
-    {},
-    {"name": "G#3", "sound": Sound("assets/g3s.wav"), },
-    {"name": "C#4", "sound": Sound("assets/c4s.wav"), },
-    {"name": "A3", "sound": Sound("assets/a3.wav"), },
-    {"name": "C4", "sound": Sound("assets/c4.wav"), },
-    {"name": "B3", "sound": Sound("assets/b3.wav"), },
-    {},
-    {"name": "A#3", "sound": Sound("assets/a3s.wav"), },
+sounds = {
+    "C2": Sound("assets/c2.wav"),
+    "C#2": Sound("assets/c2s.wav"),
+    "D2": Sound("assets/d2.wav"),
+    "D#2": Sound("assets/d2s.wav"),
+    "E2": Sound("assets/e2.wav"),
+    "F2": Sound("assets/f2.wav"),
+    "F#2": Sound("assets/f2s.wav"),
+    "G2": Sound("assets/g2.wav"),
+    "G#2": Sound("assets/g2s.wav"),
+    "A2": Sound("assets/a2.wav"),
+    "A#2": Sound("assets/a2s.wav"),
+    "B2": Sound("assets/b2.wav"),
 
-    # MUX 2
-    {"name": "F#4", "sound": Sound("assets/f4s.wav"), },
-    {"name": "D4", "sound": Sound("assets/d4.wav"), },
-    {"name": "G#4", "sound": Sound("assets/g4s.wav"), },
-    {"name": "D#4", "sound": Sound("assets/d4s.wav"), },
-    {"name": "G4", "sound": Sound("assets/g4.wav"), },
-    {},
-    {"name": "F4", "sound": Sound("assets/f4.wav"), },
-    {"name": "E4", "sound": Sound("assets/e4.wav"), },
+    "C3": Sound("assets/c3.wav"),
+    "C#3": Sound("assets/c3s.wav"),
+    "D3": Sound("assets/d3.wav"),
+    "D#3": Sound("assets/d3s.wav"),
+    "E3": Sound("assets/e3.wav"),
+    "F3": Sound("assets/f3.wav"),
+    "F#3": Sound("assets/f3s.wav"),
+    "G3": Sound("assets/g3.wav"),
+    "G#3": Sound("assets/g3s.wav"),
+    "A3": Sound("assets/a3.wav"),
+    "A#3": Sound("assets/a3s.wav"),
+    "B3": Sound("assets/b3.wav"),
 
-    # MUX 3
-    {},
-    {"name": "A4", "sound": Sound("assets/a4.wav"), },
-    {},
-    {"name": "A#4", "sound": Sound("assets/a4s.wav"), },
-    {},
-    {"name": "C5", "sound": Sound("assets/c5.wav"), },
-    {},
-    {"name": "B4", "sound": Sound("assets/b4.wav"), },
+    "C4": Sound("assets/c4.wav"),
+    "C#4": Sound("assets/c4s.wav"),
+    "D4": Sound("assets/d4.wav"),
+    "D#4": Sound("assets/d4s.wav"),
+    "E4": Sound("assets/e4.wav"),
+    "F4": Sound("assets/f4.wav"),
+    "F#4": Sound("assets/f4s.wav"),
+    "G4": Sound("assets/g4.wav"),
+    "G#4": Sound("assets/g4s.wav"),
+    "A4": Sound("assets/a4.wav"),
+    "A#4": Sound("assets/a4s.wav"),
+    "B4": Sound("assets/b4.wav"),
+
+    "C5": Sound("assets/c5.wav"),
+}
+
+muxmap = [
+    #  0      1      2       3      4      5     6      7
+    "F2",  "C2",  "G2",  "C#2", "F#2", "D#2", "E2",  "D2",  # MUX 0
+    "",    "G#2", "C#3", "A2",  "C3",  "B2",  "",    "A#2", # MUX 1
+    "F#3", "D3",  "G#3", "D#3", "G3",  "",    "F3",  "E3",  # MUX 2
+    "",    "A3",  "",    "A#3", "",    "C4",  "",    "B3",  # MUX 2
 ]
 
-print("Running...")
+# Opening sound
+def play_opening_sounds():
+    sounds["C3"].play()
+    sleep(0.25)
+    sounds["E3"].play()
+    sleep(0.25)
+    sounds["G3"].play()
+    sleep(0.25)
+    sounds["C4"].play()
 
+play_opening_sounds()
+
+print("Piano is ready.")
+
+# Main loop
 bs = [0] * 32
 while True:
-    for s in range(0, 8):
+    for s in range(8):
         b = '{0:03b}'.format(s)
         for i in range(3):
             if b[i] == '1':
@@ -79,12 +104,14 @@ while True:
             idx = i * 8 + s
 
             if mux[i].is_pressed:
-                if bs[idx] == 0:
-                    bs[idx] = 1
-                    soundmap[idx]["sound"].play()
+                if bs[idx] < 20:
+                    bs[idx] = 50
+                    sounds[muxmap[idx]].stop()
+                    sounds[muxmap[idx]].play()
             else:
-                if bs[idx] == 1:
-                    bs[idx] = 0
-                    soundmap[idx]["sound"].fadeout(250)  # stop() sounds weird, so fading them out...
+                if bs[idx] > 0:
+                    bs[idx] -= 1
+                    if bs[idx] == 0:
+                        sounds[muxmap[idx]].fadeout(250)  # stop() sounds weird, so fading them out...
 
     print(bs, end='\r')
